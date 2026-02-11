071872a64fde5948de43e3cb133752f8cae9c4f7
Luis Felix <untiveros2024@gmail.com>
2026-02-11 14:42:48 -0500

    error 2 solucion

--- src/config/redis.connection.ts
+++ src/config/redis.connection.ts
@@ -3,5 +3,6 @@ import IORedis from 'ioredis';
 export const connection = new IORedis({
     host: process.env.REDIS_HOST || 'localhost',
     port: Number(process.env.REDIS_PORT) || 6379,
+    password: process.env.REDIS_PASSWORD || undefined,
     maxRetriesPerRequest: null, // Requerido por BullMQ
 });
